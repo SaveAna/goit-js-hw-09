5d7dfc7246444166c804565ae33bb48345838e33
Anastasiia Savenok <i.wasber@gmail.com>
2023-03-25 21:55:57 +0200

    add basic const and random func (01-color-switcher)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', onBtnClick);
+stopBtn.addEventListener('click', onBtnClick);
+
+function onBtnClick() {}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
