af94d51d768b11d14f7bf4beb1d21097f90dc825
Anastasiia Savenok <i.wasber@gmail.com>
2023-03-25 22:33:19 +0200

    add color-functions (01-color-switcher ready)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,13 +1,28 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
+let colorChange = null;
 
-startBtn.addEventListener('click', onBtnClick);
-stopBtn.addEventListener('click', onBtnClick);
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
 
-function onBtnClick() {}
+function onStartClick() {
+  colorChange = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function onStopClick() {
+  clearInterval(colorChange);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+stopBtn.disabled = true;
