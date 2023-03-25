6dfa07cb638d827cb83a88876c16cedecc67ffa9
Anastasiia Savenok <i.wasber@gmail.com>
2023-03-25 23:26:12 +0200

    add notify

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,5 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// Notiflix.Notify.failure('Please choose a date in the future');
