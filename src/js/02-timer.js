cc3fac59721a0112d4d4a49fca20c04ac31f8bd8
Anastasiia Savenok <i.wasber@gmail.com>
2023-03-25 23:33:19 +0200

    add calendar

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,4 +2,19 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+const calendar = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const timer = document.querySelector('.timer');
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(calendar, options);
+
 // Notiflix.Notify.failure('Please choose a date in the future');
