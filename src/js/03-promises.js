68b0a04df8893eb031307fe44efad69fcc8eba44
Anastasiia Savenok <i.wasber@gmail.com>
2023-03-26 22:44:40 +0300

    add basic functions (03-promises)

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,12 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+const button = document.querySelector('button[type="submit"]');
+
+const delayInput = document.querySelector('input[name="delay"]');
+const stepInput = document.querySelector('input[name="step"]');
+const amountInput = document.querySelector('input[name="amount"]');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +15,25 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+// =============================================
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (form) {
+      resolve();
+    } else {
+      reject();
+    }
+  }, delayInput);
+});
