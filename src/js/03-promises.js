6ec840ad198cc234c5912fc3d2e8aaef84ce9fc8
Anastasiia Savenok <i.wasber@gmail.com>
2023-03-26 22:49:17 +0300

    change createPromise func

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,19 +1,18 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('.form');
-const button = document.querySelector('button[type="submit"]');
-
-const delayInput = document.querySelector('input[name="delay"]');
-const stepInput = document.querySelector('input[name="step"]');
-const amountInput = document.querySelector('input[name="amount"]');
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
 
 createPromise(2, 1500)
@@ -27,13 +26,3 @@ createPromise(2, 1500)
   });
 
 // =============================================
-
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (form) {
-      resolve();
-    } else {
-      reject();
-    }
-  }, delayInput);
-});
